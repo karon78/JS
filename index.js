d5b8703e954b2245fb0003b334884e7160d1baae
karon78 <karon78@gmail.com>
2020-06-19 13:59:15 +0300

    Added functions for calculating the square of geometric shapes: rhombus, cylinder, triangle, rectangle

--- index.js
+++ index.js
@@ -27,3 +27,53 @@ return a % b === 0;
 function triangle( a , b , c ) {
 return a + b > c && b + c > a && c + a > b ;
 }
+
+            // square
+
+// square rhombus
+/**
+ *
+ * @param {number} side
+ * @param {number} height
+ * @returns {number}
+ */
+function calculateSquareRhombus1( side , height ) {
+return side * height;
+}
+
+/**
+ *
+ * @param {number} diagonal1
+ * @param {number} diagonal2
+ * @returns {number}
+ */
+function calculateSquareRhombus2( diagonal1 , diagonal2) {
+return diagonal1 * diagonal2 / 2 ;
+}
+// square cylinder
+
+function calculateSquareCylinder( radius , height ) {
+return 2 * Math.PI * radius * (height + radius);
+}
+
+// square triangle
+/**
+ *
+ * @param {number} side
+ * @param {number} height
+ * @returns {number}
+ */
+function calculateSquareTriangle( side , height ) {
+return side * height / 2 ;
+}
+
+// square rectangle
+/**
+ *
+ * @param {number} width
+ * @param {number} length
+ * @returns {number}
+ */
+function calculateSquareRectangle( width , length ) {
+return width * length ;
+}
